63b4edda70cb90b7353866b474c72208491524cc
Ojeda2001 <ojeda2001richard@gmail.com>
2021-06-02 12:19:53 -0500

    t

---
+++ src/ec/edu/utpl/ep1/Banco/Corriente.java
@@ -0,0 +1,55 @@
+package ec.edu.utpl.ep1.Banco;
+
+
+    /**
+     * @author Edgar MartÃ­nez y Richard Ojeda
+     * Esta clase sirve para determinar componentes propios de una cuenta de banco corriente
+     */
+
+    public class Corriente extends Cuenta{
+        private int nroMaxCheques;
+        private SecuencialCheque secuenciaCh;
+
+        /**
+         * @param saldo
+         * @param numero
+         * Constructor para la inicializar una cuenta de banco corriente
+         */
+        public Corriente(double saldo, String numero) {
+            super(saldo, numero);
+        }
+
+        /**
+         * Se encarga de mostrar el valor de nroMaxCheques
+         * @return nroMaxCheques
+         */
+        public int getNroMaxCheques() {
+            return nroMaxCheques;
+        }
+
+        /**
+         *Se encarga de asignar un valor a nroMaxCheques
+         * @param nroMaxCheques
+         */
+        public void setNroMaxCheques(int nroMaxCheques) {
+            this.nroMaxCheques = nroMaxCheques;
+        }
+
+        /**
+         *Se encarga de mostrar la secuencia de los cheques de secuenciaCh
+         * @return secuenciaCh
+         */
+        public SecuencialCheque getSecuenciaCh() {
+            return secuenciaCh;
+        }
+
+        /**
+         *Se encarga de asignar un valor a la secuencia de cheques de secuenciaCh
+         * @param secuenciaCh
+         */
+        public void setSecuenciaCh(SecuencialCheque secuenciaCh) {
+            this.secuenciaCh = secuenciaCh;
+        }
+    }
+
+}
